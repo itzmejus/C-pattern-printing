75a7210d32b5556304f7da4e87084cbbcf3aafc3
itzmejus <itzjustin3344@gmail.com>
2022-02-02 12:36:21 +0530

    Diamond pattern

---
+++ p28.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+void main()
+{
+    int i,j,m=1,k,n=4;
+    for(i=1;i<=7;i++)
+    {   
+        //loop to print space
+        for(j=1;j<n;j++)
+        {
+            printf("  ");
+        }
+        //loop to print *
+        for(k=1;k<m*2;k++)
+        {
+            printf(" X");
+        }
+        printf("\n");
+        if(i<4)
+        {
+            n--;
+            m++;
+        }
+        else
+        {
+            n++;
+            m--;
+        }
+    }
+}
+
+
+/* 
+
+       X
+     X X X
+   X X X X X
+ X X X X X X X
+   X X X X X
+     X X X
+       X
+       
+ */
